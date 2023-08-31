88a742919edcae3449a5d56a4b99dfa4449e1e85
muhammadhashirsafdar <139388035+muhammadhashirsafdar@users.noreply.github.com>
2023-09-01 01:33:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+
+
+               // ! Chapter 1 (Alert) ! \\
+
+
+                  
+
+// ? Question 1 
+
+// alert("Assalaoalium");
+
+// ?  Question 2 
+
+// alert("Error! Please enter a valid password");
+
+
+// ? Question 3
+
+// alert("Welcome to JS Land...\nHappy Coming!");
+
+
+// ? Question 4
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coming!");
+
+
+
+// ? Question 5
+
+// console.log("Hello... I can run JS through my webbrowser's console");
+
+//  Question 6
+//  Question 7
+
+
+         // ! Chapter 2 (VARIABLES FOR STRING) ! \\
+                
+        
+
+// ? Question 1 
+
+// var username ="username";
+// alert(username);
+
+// ? Question 2
+
+// var myNAME ="Hashir Safdar";
+// alert(myNAME);
+
+// ? Question 3
+
+// var message ="Hello World";
+// alert(message);
+
+// ? Question 4
+
+// var NAME ="jhone doe";
+// var age ="15 years old";
+// var certificate ="certified mobile application development";
+ 
+// alert(NAME);
+// alert(age);
+// alert(certificate);
+
+// ? Question 5
+
+// var cel = "pizza\npizz\npiz\npi\np";
+// alert(cel);
+
+// ? Question 6
+
+// var email ="example@example.com";
+// alert(email);
+
+// ? Question 7
+
+// var book ="I am trying to learn from the book a smarter\nway to learn javascript";
+// alert(book);
+
+// ? Question 8
+
+// var pen="Yah! I can write html content through javascript";
+// document.write(pen);
+
+// ? Question 9
+
+// var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+
+// document.write(design);
